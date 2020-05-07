c9f7c246d13a4e2386e0675b70969fcf95d9bfca
KAZI SAHARIAR RAHI <srahi00000@gmail.com>
2020-05-07 23:03:14 +0530

    food add to restaurant

--- app/src/main/java/org/example/foodie/models/Order.java
+++ app/src/main/java/org/example/foodie/models/Order.java
@@ -1,90 +1,38 @@
 package org.example.foodie.models;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Order {
-
 
     public double totalPrice;
     @SerializedName("user")
     public User user;
     @SerializedName("restaurant")
     Restaurant restaurant;
-    @SerializedName("payment")
-    Payment payment;
     @SerializedName("_id")
     String _id;
-    @SerializedName("foods")
-    List<OrderFood> foodList = new ArrayList<>();
-
     @SerializedName("restaurantId")
     String restaurantId;
 
     List<Restaurant> restaurantList;
 
-    public Order(String restaurantId, List<OrderFood> foodList, Payment payment) {
-        this.payment = payment;
-        this.foodList = foodList;
-        this.restaurantId = restaurantId;
+
+    public Order() {
     }
-
-
-    public Payment getPayment() {
-        return payment;
-    }
-
 
     public User getUser() {
         return user;
     }
-
     public double getTotalPrice() {
         return totalPrice;
     }
-
-    public List<OrderFood> getFoodList() {
-        return foodList;
-    }
-
     public String getRestaurantId() {
         return restaurantId;
     }
-
     public List<Restaurant> getRestaurantList() {
         return restaurantList;
     }
-
-    /*
-    foods	[...]
-    restaurant	{...}
-    user	{...}
-    deliveryGuy	{...}
-    status	string
-    Status of the order- RECIEVED/ LEFT/ DELIVERED/ CANCELLED
-
-    payment*	{
-        description:
-        Details of payment
-
-        method	string
-        Mode of payment- COD/ UPI/ CARD
-
-        status	string
-        Payment status- PAID/ UNPAID
-
-        total	string
-        Total amount to be paid
-
-    }
-}*/
 }
-
-
