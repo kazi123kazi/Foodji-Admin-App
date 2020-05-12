1b94a67968e9bec1061b499c31ef33bbb69a30a6
Afnan khan <khan.afnan1117@gmail.com>
2020-05-12 23:03:49 +0530

    Added Image posting route for Restaurant fix

---
+++ app/src/main/java/org/example/foodie/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package org.example.foodie;
+
+import android.content.ContentProvider;
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+}
---
+++ app/src/main/java/org/example/foodie/viewmodels/RestaurantsViewModel.java
@@ -0,0 +1,31 @@
+package org.example.foodie.viewmodels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import org.example.foodie.Respositories.UserRepository;
+import org.example.foodie.Respositories.UserRepository;
+import org.example.foodie.models.ResponseUser;
+import org.example.foodie.models.Restaurant;
+
+import java.util.List;
+
+public class RestaurantsViewModel extends ViewModel {
+
+    private MutableLiveData<ResponseUser> mutableLiveData;
+    private UserRepository userRepository;
+
+    public void init() {
+        if (mutableLiveData != null) {
+            return;
+        }
+        userRepository = new UserRepository().getInstance();
+        mutableLiveData = userRepository.getUser();
+    }
+
+    public LiveData<ResponseUser> getRestaurantRepository() {
+        return mutableLiveData;
+    }
+
+}
